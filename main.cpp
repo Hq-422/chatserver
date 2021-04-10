3010a522001fa4b6419de099fdda1cebce7d7d00
Hq-422 <1259692659@qq.com>
2021-04-10 10:48:25 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 void Sort(int arr[], int n)
 {
+	int tmp = 0;
+	for(int i = 0;i < n - 1;++i)
+	{
+		for(int j = 0;j < n - 1 - i;++j)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
 
+		}
+	}
 }
 
 int main()
