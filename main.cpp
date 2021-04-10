ab6e1d89b8ecc53d2aeb7f3f330b9409ca553872
Hq-4222 <1259692659@qq.com>
2021-04-10 10:26:50 +0800

    创建main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void Sort(int arr[], int n)
+{
+
+}
+
+int main()
+{
+	int arr[] = { 2,2,2,3,2,4,2,6,7,8,2,2,2,2 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	Sort(arr, size);
+
+	return 0;
+}
